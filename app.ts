10b010c05bc1b5e9a6be84c630e043f6a511e9b2
GaijinSoft <40780484+GaijinSoft@users.noreply.github.com>
2018-07-02 22:11:39 -0400

    Init

---
+++ app.ts
@@ -0,0 +1,48 @@
+/// <reference path="phaser.d.ts"/>
+
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    const game = new Phaser.Game(config);
+    var platforms;
+
+    function preload ()
+    {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+
+        // TODO: Left off at https://phaser.io/tutorials/making-your-first-phaser-3-game/part3
+    }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'sky');
+        platforms = this.physics.add.staticGroup();
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+        // this.add.spritesheet('dude');
+    }
+
+    function update ()
+    {
+    }
