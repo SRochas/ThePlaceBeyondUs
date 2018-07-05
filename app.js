f85847bdf570e437c055c372966306eb062c5b03
GaijinSoft <40780484+GaijinSoft@users.noreply.github.com>
2018-07-05 17:49:40 -0400

    Tutorial up to Part 8 - Stardust https://phaser.io/tutorials/making-your-first-phaser-3-game/part8

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var config = {
         update: update
     }
 };
-var game = new Phaser.Game(config);
 var platforms;
+var player;
+var game = new Phaser.Game(config);
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
@@ -33,7 +34,43 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
-    // this.add.spritesheet('dude');
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.physics.add.collider(player, platforms);
 }
 function update() {
+    var cursors /*: Phaser.Input.Keyboard.KeyboardPlugin*/ = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
